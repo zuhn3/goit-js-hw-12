eb4771bf4db677214eda9ff395c39297ca6f3fc8
zuhn3 <aop1603@gmail.com>
2025-04-23 19:47:36 +0100

    hw 12

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: "vite build --base=/goit-js-hw-12/",
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
